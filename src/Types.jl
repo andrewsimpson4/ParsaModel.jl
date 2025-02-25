fe2a40825cf0d42cc33def9c1a9881f32cc3faf9
Andrew Simpson <ajsimps@me.com>
2025-02-25 10:02:46 -0600

    Types file

---
+++ src/Types.jl
@@ -0,0 +1,241 @@
+
+
+mutable struct Unknown
+    value::Int
+end
+Unknown() = Unknown(0)
+
+mutable struct Parsa_index
+    value :: Any
+end
+
+Base.@kwdef mutable struct CategoricalZset
+    set = Dict()
+end
+
+Base.@kwdef mutable struct CategoricalZ
+    K::Int
+    LV = Dict{Any, LatentVaraible}()
+    Pi::Vector{Real} = ones(K) ./ K
+end
+
+
+Base.@kwdef mutable struct LatentVaraible
+    value_::Union{Int,Unknown}
+    Z::CategoricalZ
+end
+
+Base.@kwdef mutable struct CategoricalZVec
+    set::Vector{Union{CategoricalZ, LatentVaraible}}
+end
+
+
+lv_v(LV::LatentVaraible) = typeof(LV.value_) == Unknown ? LV.value_.value : LV.value_
+lv_set(LV::LatentVaraible, x) = typeof(LV.value_) == Unknown ? LV.value_.value = x : nothing
+lv_isKnown(LV::LatentVaraible) = typeof(LV.value_) == Unknown ? (lv_v(LV) == 0 ? false : true) : true
+
+function get_possible_indexes(V::Vector{Any}, i::Int)
+
+    L = length(V)
+    next = V[i]
+    if typeof(next) == Parsa_index
+        next = [next]
+    end
+    ve = Vector{Any}(undef, 0)
+    if i == L
+        ve = [[n] for n in next]
+    else
+        for n in next
+            ind_sets = get_possible_indexes(V, i+1)
+            ve = [ve; [[n; ind] for ind in ind_sets]]
+        end
+    end
+
+    return ve
+end
+
+function Base.getindex(PG::CategoricalZVec, indx...)
+    indx = collect(indx)
+    [typeof(P) == CategoricalZ ? P[indx] : P for P in PG.set]
+end
+
+function Base.getindex(PG::CategoricalZ, indx...)
+    indx = collect(indx)
+    V = Vector{Any}(undef, length(indx))
+    for (i, ix) in enumerate(indx)
+        if typeof(ix) == LatentVaraible
+
+            if lv_v(ix) == 0
+                V[i] = 1:ix.Z.K
+            else
+                V[i] = lv_v(ix)
+            end
+        else
+            V[i] = ix
+        end
+    end
+    all_indx = get_possible_indexes(V, 1)
+    LV = Vector{LatentVaraible}(undef, length(all_indx))
+    for (i, v) in enumerate(all_indx)
+        if haskey(PG.LV, v)
+            LV[i] = PG.LV[v]
+        else
+            PG.LV[v] = LatentVaraible(PG)
+            LV[i] = PG.LV[v]
+        end
+    end
+    if length(LV) == 1
+        return LV[1]
+    else
+        return LV
+    end
+end
+
+function Base.getindex(PG::CategoricalZset, indx...)
+    indx = collect(indx)
+    V = Vector{Any}(undef, length(indx))
+    extra_LV = Vector{LatentVaraible}()
+    for (i, ix) in enumerate(indx)
+        if typeof(ix) == LatentVaraible
+            if lv_v(ix) == 0
+                V[i] = 1:ix.Z.K
+                extra_LV = [extra_LV; ix]
+            else
+                V[i] = lv_v(ix)
+            end
+        else
+            V[i] = indx
+        end
+    end
+    all_indx = get_possible_indexes(V, 1)
+    LV = Vector{CategoricalZ}(undef, length(all_indx))
+    for (i, v) in enumerate(all_indx)
+        LV[i] = PG.set[v]
+    end
+    LV = [LV; extra_LV]
+    if length(LV) == 1
+        return LV[1]
+    else
+        return CategoricalZVec(LV)
+    end
+end
+
+
+
+function Base.setindex!(Z::CategoricalZ, LV::LatentVaraible, i::Int64...)
+    Z.LV[collect(i)] = LV
+end
+
+LatentVaraible(Z::CategoricalZ) = LatentVaraible(value_ = Unknown(), Z= Z)
+LatentVaraible(Z::CategoricalZ, value::Int) = LatentVaraible(value_ = value, Z= Z)
+
+function sampleZ(Z::CategoricalZ; value=nothing)
+    if isnothing(value)
+        LatentVaraible(Z)
+    else
+        LatentVaraible(Z, value)
+    end
+end
+
+Base.@kwdef mutable struct ParameterValue
+    value::Any
+    n_parameters = length(value)
+end
+
+function compress_package(X, taus, parameter_maps)
+    map_new = [Dict([key => vv.value.value for (key,vv) in params]) for params in parameter_maps]
+    x_new = [x.X for x in X]
+    zip(x_new, taus, map_new)
+end
+
+Base.@kwdef mutable struct Parameter
+    value::ParameterValue
+    update::Function
+    run_update::Function = (X, taus, parameter_maps, log_pdf) -> value.value = update(value.value, compress_package(X, taus, parameter_maps), log_pdf)
+    is_const::Bool = false
+end
+
+Parsa_Parameter(initial_value, update_function) = Parameter(value = ParameterValue(value = initial_value), update = update_function)
+Parsa_Parameter(initial_value, number_of_parameters, update_function) = Parameter(value = ParameterValue(value = initial_value, n_parameters = number_of_parameters), update = update_function)
+
+Base.@kwdef mutable struct ParameterGenerator
+    parameter_base :: Parameter
+    parameter_map :: Dict = Dict()
+end
+
+function Base.getindex(PG::ParameterGenerator, indx...)
+    indx = collect(indx)
+    if size(indx) == ()
+        indx = indx[1]
+    end
+    if typeof(indx) == Vector{Array{Int64, 0}}
+        indx = [indx[1][1]]
+    end
+    if haskey(PG.parameter_map, indx)
+        return PG.parameter_map[indx]
+    else
+        PG.parameter_map[indx] = deepcopy(PG.parameter_base)
+        return PG.parameter_map[indx]
+    end
+end
+
+function index_to_parameter_values(p, parameters)
+    new = Dict()
+    for (key, indx) in p
+        V = indx
+        if typeof(V) == LatentVaraible
+            V = lv_v(indx)
+        else
+            V = [typeof(v) == LatentVaraible ? lv_v(v) : v for v in indx]
+        end
+        new[key] = parameters[key][V].value.value
+    end
+    return new
+end
+
+function index_to_parameters(p, parameters)
+    new = Dict()
+    for (key, indx) in p
+        V = indx
+        if typeof(V) == LatentVaraible
+            V = lv_v(indx)
+        else
+            V = [typeof(v) == LatentVaraible ? lv_v(v) : v for v in indx]
+        end
+        new[key] = parameters[key][V]
+    end
+    return new
+end
+
+function parameter_map_to_values(p)
+    new = Dict()
+    for (key, val) in p
+        new[key] = val.value.value
+    end
+    return new
+end
+
+
+Base.@kwdef mutable struct LMMM_Base
+    pdf::Function
+    log_pdf::Function
+    parameters::Dict
+    parameter_order :: Vector
+    evaluate::Function = (X, p) -> pdf(X.X, index_to_parameter_values(p, parameters))
+    eval_catch = Dict()
+end
+
+Parsa_density(pdf, log_pdf, params...) = LMMM_Base(pdf = pdf, log_pdf = log_pdf,
+                                                   parameter_order = [k for (k,_) in collect(params)],
+                                                   parameters = Dict([key => ParameterGenerator(parameter_base = vv) for (key, vv) in Dict(params...)]))
+
+
+struct T
+    domain::Vector{Function}
+    map::Function
+end
+
+mutable struct Observation
+    X::Any
+    T::T
+end
